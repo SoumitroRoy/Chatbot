409a1fc60fb566b101d9d59e7f275ba121608062
Soumitro Roy <soumitro.roy06@gmail.com>
2020-12-24 01:17:22 +0600

    importing frame, panel, textarea and scrollpane

--- Chatbot.java/src/chatbot/java/ChatbotJava.java
+++ Chatbot.java/src/chatbot/java/ChatbotJava.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package chatbot.java;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
-/**
- *
- * @author Pc
- */
-public class ChatbotJava {
+import java.awt.Color;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
